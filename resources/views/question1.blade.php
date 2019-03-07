d2ed154c778bb1f4d264463ae04854dad39e79e3
Ali <jamaliali219@gmail.com>
2019-03-07 13:16:39 +0330

    Add basic score form

--- resources/views/question1.blade.php
+++ resources/views/question1.blade.php
@@ -29,6 +29,7 @@
     <style type="text/css">
         body {
             background-image: linear-gradient(45deg, #ee2e3b, #ffd300);
+            background-attachment: fixed;
             /*background-image: linear-gradient(45deg, #ffb953, #f226e1);*/
             /*background-image: linear-gradient(45deg, #76b3e4, #b76c7a);*/
             /*background-image: linear-gradient(45deg, #ffe88e, #e49ca8);*/
@@ -121,5 +122,66 @@
         </div>
     </div>
 </div>
+
+
+<div class="ui middle aligned center aligned grid">
+    <div class="column">
+        <h2 class="ui teal image header">
+            <div class="content iransans" style="color: white;">
+                دریافت امتیازات
+            </div>
+        </h2>
+        <form class="ui large form" id="form" action="/judgment" method="post">
+            <div class="ui stacked segment">
+                <div class="field required">
+                    <label style="text-align: right">نام کاربری</label>
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <input class="iransans" type="text" name="username" placeholder="نام کاربری">
+                    </div>
+                </div>
+                <div class="field required">
+                    <label style="text-align: right">کلمه عبور</label>
+                    <div class="ui left icon input">
+                        <i class="lock icon"></i>
+                        <input class="iransans" type="password" name="password" placeholder="کلمه عبور">
+                    </div>
+                </div>
+                <div class="field">
+                    <label style="text-align: right">کد تیم</label>
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <input class="iransans" type="text" name="team_code" placeholder="کد تیم">
+                    </div>
+                </div>
+                <div class="field">
+                    <label style="text-align: right">کد چالش</label>
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <select name="challenge_code" class="ui dropdown">
+                            <option class="iransans" value="Java">Java</option>
+                            <option class="iransans" value=".net">Net</option>
+                            <option class="iransans" value="Php">PHP</option>
+                            <option class="iransans" value="Python">Python</option>
+                            <option class="iransans" value="Image">Image</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="ui fluid large orange submit button iransans" style="margin: 0"
+                     onclick="document.getElementById('form').submit();">ارسال
+                </div>
+            </div>
+
+            <div class="ui error message"></div>
+
+        </form>
+
+        <div class="ui message">
+            ICT Challenge, 2019
+        </div>
+    </div>
+</div>
+
+
 </body>
 </html>
